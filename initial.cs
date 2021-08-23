9e886e74b9331deb63218f3345a9920f339bfb64
MIkhail Sharov <msharov2017@gmail.com>
2021-08-22 21:09:44 -0400

    Update initial.cs

--- initial.cs
+++ initial.cs
@@ -6,9 +6,19 @@ namespace Simple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Player 1 name:");
-            string player1 = Console.ReadLine();
-            Console.WriteLine("Player 1 is named: " + player1 + " is this correct?");
+            Console.WriteLine("Welcome to Inis! How many players are playing today?");
+            int numplayers = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while(i <= numplayers)
+            {
+              Console.WriteLine("Enter Player " + i.ToString() + " name:");
+              string playername = Console.ReadLine();
+              Console.WriteLine("Player " + i.ToString() + " is named: " + playername + " is this correct? (y/n)");
+              string response = Console.ReadLine();
+              if (response == "y") {
+                i++;
+              }
+            }
         }
     }
 }
